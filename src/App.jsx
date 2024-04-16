7c8cb8b40477cbbf1498271da129f893975b5b0a
Lucas Jaime <lucasgabrieljaime99@gmail.com>
2024-04-16 03:14:05 -0300

    agregamos el componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import Footer from "./components/common/Footer";
+import Menu from "./components/common/Menu";
+import Administrador from "./components/pages/Administrador";
+import Inicio from "./components/pages/Inicio";
+import Error404 from "./components/pages/Error404";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormularioHabitacion from "./components/pages/habitacion/FormularioHabitacion";
+import DetalleHabitacion from "./components/pages/DetalleHabitacion";
+import Login from "./components/pages/Login";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdmin from "./components/routes/RutasAdmin";
+import { useState } from "react";
+import Registro from "./components/pages/Registro";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const usuario = JSON.parse(sessionStorage.getItem('RChotel')) || ""
+  const [usuarioLogueado , setUsuarioLogueado] = useState(usuario); 
   return (
-    <>
-    <h1>titulo de prueba</h1>
-    </>
-  )
+   
+    <BrowserRouter>
+      <Menu usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Menu>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route
+          exact
+          path="/detalleHabitacion/:id"
+          element={<DetalleHabitacion></DetalleHabitacion>}
+        ></Route>
+        <Route exact path="/registro" element={Registro}></Route>
+        <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}></Route>
+        <Route
+          exact
+          path="/administrador/*"
+          element={
+            <RutasProtegidas>
+              <RutasAdmin></RutasAdmin>
+            </RutasProtegidas>
+          }
+        ></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
