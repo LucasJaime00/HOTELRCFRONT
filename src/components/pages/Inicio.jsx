5126a0cd95b4597a62507fabae466e500c3c761b
Lucas Jaime <lucasgabrieljaime99@gmail.com>
2024-04-16 03:00:28 -0300

    agregamos el componente de inicio

---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,53 @@
+import { Container, Row } from "react-bootstrap";
+import CardHabitacion from "./habitacion/CardHabitacion";
+import { useEffect, useState } from "react";
+import { leerHabitacionesAPI } from "../../helpers/queries";
+
+const Inicio = () => {
+  const [habitaciones, setHabitaciones] = useState([]);
+
+  useEffect(() => {
+    obtenerHabitaciones();
+  }, []);
+
+  const obtenerHabitaciones = async () => {
+    const respuesta = await leerHabitacionesAPI();
+    if (respuesta.status === 200) {
+      
+      const datos = await respuesta.json();
+      setHabitaciones(datos);
+    } else {
+      Swal.fire({
+        title: "Ocurrio un error",
+        text: `Intenta esta operación en unos minutos.`,
+        icon: "error",
+      });
+    }
+  };
+
+  return (
+    <>
+      <img
+        className="banner"
+        src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="fondo de hotel"
+      />
+      <Container className="mainContainer my-4">
+        <section>
+          <h2 className="display-3">Nuestros habitaciones</h2>
+          <hr />
+          <Row>
+            {habitaciones.map((habitacion) => (
+              <CardHabitacion
+              habitacion={habitacion}
+                key={habitacion._id}
+              ></CardHabitacion>
+            ))}
+          </Row>
+        </section>
+      </Container>
+    </>
+  );
+};
+
+export default Inicio;
