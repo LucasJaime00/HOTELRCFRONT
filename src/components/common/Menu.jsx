1bffefdb74b9f887ce30497df8ecaf063b8f41bd
Lucas Jaime <lucasgabrieljaime99@gmail.com>
2024-04-16 02:34:53 -0300

    agregamos el menu

---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,54 @@
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/";
+const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
+
+  const logout = ()=>{
+  
+    sessionStorage.removeItem('RChotel')
+    
+    setUsuarioLogueado("")
+  }
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            alt="Logo "
+            className="img-fluid"
+            width={150}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink end to="/" className="nav-link">
+              Inicio
+            </NavLink>
+            <NavLink end to="/registro" className="nav-link">
+              Registro
+            </NavLink>
+            {
+              
+              usuarioLogueado.length > 0 ? (
+                <>
+                  <NavLink end to="/administrador" className="nav-link">
+                    Administrador
+                  </NavLink>
+                  <Button onClick={logout}>Logout</Button>
+                </>
+              ) : (
+                <NavLink end to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              )
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Menu;
